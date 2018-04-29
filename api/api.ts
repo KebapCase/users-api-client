8b5b60ca281193a9ff1a471776cb72815c5c478c
vtrusevich <vtrusevich@gmail.com>
2018-04-29 14:29:08 +0300

    new version

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,7 @@
 export * from './authenticationManagement.service';
 import { AuthenticationManagementService } from './authenticationManagement.service';
+export * from './eventManagement.service';
+import { EventManagementService } from './eventManagement.service';
 export * from './userManagement.service';
 import { UserManagementService } from './userManagement.service';
-export const APIS = [AuthenticationManagementService, UserManagementService];
+export const APIS = [AuthenticationManagementService, EventManagementService, UserManagementService];
--- generator/generator.sh
+++ generator/generator.sh
@@ -1,4 +1,2 @@
-java -jar swagger-codegen-cli.jar generate \
--i http://localhost:8080/swagger.json?basePath=http%3A%2F%2Flocalhost%3A8080 \
--l typescript-angular \
--o ../
+java -jar generator/swagger-codegen-cli.jar generate -i http://localhost:8080/swagger.json?basePath=http%3A%2F%2Flocalhost%3A8080 -l typescript-angular
+
